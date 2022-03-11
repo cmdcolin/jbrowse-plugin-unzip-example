9d69728df12fc7c6d33a53c9bfa6d6628f9e3801
Colin <colin.diesh@gmail.com>
2022-03-11 14:05:18 -0700

    Use bgzf-filehandle

--- src/HelloView/components/HelloView.tsx
+++ src/HelloView/components/HelloView.tsx
@@ -1,7 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { openLocation } from '@jbrowse/core/util/io'
+import { unzip } from '@gmod/bgzf-filehandle'
 
 export default function ReactComponent() {
   const [pushed, setPushed] = useState('')
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const res = await openLocation({
+          uri: 'test_data/simple.vcf.gz',
+          locationType: 'UriLocation',
+        }).readFile()
+
+        console.log({ res })
+        const r = await unzip(res)
+        console.log({ r }, r.toString())
+      } catch (e) {
+        console.error(e)
+      }
+    })()
+  }, [])
   return (
     <div style={{ padding: 50 }}>
       <h1>Hello plugin developers!</h1>
